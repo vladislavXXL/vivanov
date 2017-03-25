098f72aaf1b1f02afde124174da9b25fc5d237a1
vladislavXXL <vitya39@gmail.com>
2017-03-26 00:26:49 +0200

    Class RotateArray for array rotating

---
+++ chapter_001/src/main/java/ru/job4j/array/RotateArray.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+/**
+* Class RotateArray to 90 degrees array rotate.
+* @author vivanov
+* @version 1
+* @since 1
+*/
+
+public class RotateArray {
+
+	/**
+	* @param array - array to rotate
+	* @return rotated array
+	*/
+	public int[][] rotate(int[][] array) {
+		int temp = 0;
+		int len = array.length;
+        for (int i = len; i >= len - 1; i--) {
+            temp = array[0][len - i];
+            array[0][len - i] = array[i - 1][0];
+            array[i - 1][0] = array[len - 1][i - 1];
+            array[len - 1][i - 1] = array[len - i][len - 1];
+            array[len - i][len - 1] = temp;
+        }
+		return array;
+	}
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/RotateArrayTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* RotateArray Test.
+* @author vivanov
+* @version 1
+* @since 26.03.2017
+*/
+
+public class RotateArrayTest {
+
+	/**
+	* Test array {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}.
+	*/
+	@Test
+	public void whenRotateThreeRowThreeColArrayThenRotatedArray() {
+		RotateArray obj = new RotateArray();
+		int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+		int[][] result = obj.rotate(arr);
+		int[][] expected = {{7, 4, 1}, {8, 5, 2}, {9, 6, 3}};
+		assertThat(result, is(expected));
+	}
+
+		/**
+	* Test array {{1, 2}, {3, 4}}.
+	*/
+	@Test
+	public void whenRotateTwoRowTwoColArrayThenRotatedArray() {
+		RotateArray obj = new RotateArray();
+		int[][] arr = {{1, 2}, {3, 4}};
+		int[][] result = obj.rotate(arr);
+		int[][] expected = {{4, 3}, {2, 1}};
+		assertThat(result, is(expected));
+	}
+
+}
