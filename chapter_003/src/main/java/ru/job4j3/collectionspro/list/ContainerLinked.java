c9335c502255fd29c42373ae2800f14562f33a8b
vladislavXXL <vitya39@gmail.com>
2018-06-17 01:32:41 +0200

    5.3.2. Создать контейнер на базе связанного списка  [#159]

---
+++ chapter_003/src/main/java/ru/job4j3/collectionspro/list/ContainerLinked.java
@@ -0,0 +1,149 @@
+package ru.job4j3.collectionspro.list;
+
+import java.util.Iterator;
+import java.util.ConcurrentModificationException;
+import java.util.NoSuchElementException;
+
+/**
+ * Class ContainerLinked based on LinkedList.
+ * @author vivanov
+ * @version 1
+ * @since 14.06.2018
+ * @param <E> type parameter
+ */
+public class ContainerLinked<E> implements Iterable<E> {
+
+    /** Field container size.*/
+    private int size;
+
+    /** First element.*/
+    private Node<E> first;
+
+    /** Last element.*/
+    private Node<E> last;
+
+    /** Container modification counter.*/
+    private int modCount = 0;
+
+    /**
+     * Add new element.
+     * @param value new element value
+     */
+    public void add(E value) {
+        Node<E> newLink = new Node<>(value);
+        if (this.size == 0) {
+            this.first = newLink;
+        } else {
+            this.last.next = newLink;
+        }
+        this.last = newLink;
+        this.size++;
+        this.modCount++;
+    }
+
+    /**
+     * Return element by index.
+     * @param index index element to find
+     * @return element value
+     */
+    public E get(int index) {
+        Node<E> result = this.first;
+        for (int i = 0; i <= index; i++) {
+            if (i == index) {
+                result = result.next;
+                if (index == 0) {
+                    result = this.first;
+                }
+                break;
+            }
+        }
+        return result.data;
+    }
+
+    /**
+     * Class Node to store data.
+     * @param <E> type parameter
+     */
+    private class Node<E> {
+        /** Some data to store in the LinkedContainer.*/
+        private E data;
+
+        /** Next element in the LinkedContainer.*/
+        private Node<E> next;
+
+        /**
+         * Node constructor.
+         * @param value data value
+         */
+        Node(E value) {
+            this.data = value;
+        }
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return new LinkedIterator(this.modCount, this.first);
+    }
+
+    /**
+     * Class LinkedIterator realisation of Iterator.
+     * @param <E> type parameter
+     */
+    private class LinkedIterator<E> implements Iterator<E> {
+
+        /** Iterator index.*/
+        private int indexItr = 0;
+
+        /** Current iterator value.*/
+        private Node<E> itrElem;
+
+        /** Saved value of the container modification operation counter.*/
+        private int expectedModCount;
+
+        /**
+         * LinkedIterator constructor.
+         * @param counter modification counter
+         * @param firstElem first element
+         */
+        private LinkedIterator(int counter, Node<E> firstElem) {
+            this.expectedModCount = counter;
+            this.itrElem = firstElem;
+        }
+
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         * @return {@code true} if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext() {
+            return this.indexItr < size ? true : false;
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         * @throws ConcurrentModificationException if the container modified
+         */
+        @Override
+        public E next() {
+            if (this.expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+            if (hasNext()) {
+                E result = this.itrElem.data;
+                this.itrElem = this.itrElem.next;
+                this.indexItr++;
+                return result;
+            } else {
+                throw new NoSuchElementException();
+            }
+        }
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j3/collectionspro/list/ContainerLinkedTest.java
@@ -0,0 +1,71 @@
+package ru.job4j3.collectionspro.list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class to test ContainerLinked.
+ * @author vivanov
+ * @version 1
+ * @since 14.06.2018
+ */
+public class ContainerLinkedTest {
+
+    /** Field ContainerLinked to store linked list of objects.*/
+    private ContainerLinked<String> obj;
+
+    /** Field iterator.*/
+    private Iterator itr;
+
+    /** Container iterator to check ConcurrentModificationException.*/
+    private Iterator itrExcept;
+
+    /**
+     * Setup before tests begins.
+     */
+    @Before
+    public void setUp() {
+        obj = new ContainerLinked<>();
+        obj.add("value1");
+        obj.add("value2");
+        itr = obj.iterator();
+    }
+
+    /**
+     * Method to check add operation and iterator.
+     */
+    @Test
+    public void checkAddElementAndIterator() {
+        assertThat(true, is(itr.hasNext()));
+        assertThat("value1", is(itr.next()));
+        assertThat(true, is(itr.hasNext()));
+        assertThat("value2", is(itr.next()));
+        assertThat(false, is(itr.hasNext()));
+    }
+
+    /**
+     * Method to check get operation.
+     */
+    @Test
+    public void checkGetElementByIndex() {
+        assertThat(obj.get(0), is("value1"));
+        assertThat(obj.get(1), is("value2"));
+    }
+
+    /**
+     * Method to check ConcurrentModificationException if container structure was modified.
+     */
+    @Test(expected = ConcurrentModificationException.class)
+    public void checkConcurrentModificationException() {
+        itrExcept = obj.iterator();
+        obj.add("value3");
+        assertThat(itrExcept.hasNext(), is(true));
+        assertThat(itrExcept.next(), is("value3"));
+    }
+}
