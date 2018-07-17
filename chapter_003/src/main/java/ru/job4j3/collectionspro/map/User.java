a453a9e07368b2241ce98acd90a2f558cd434fa1
vladislavXXL <vitya39@gmail.com>
2018-07-17 23:48:04 +0200

    4. Переопределить только equals [#1004]

--- chapter_003/src/main/java/ru/job4j3/collectionspro/map/User.java
+++ chapter_003/src/main/java/ru/job4j3/collectionspro/map/User.java
@@ -34,11 +34,38 @@ public class User {
      * Method hashCode.
      * @return int value
      */
+    /*
     @Override
     public int hashCode() {
         int result = name.hashCode();
         result = 31 * result + children;
         result = 31 * result + birthday.hashCode();
         return result;
+    }
+    */
+
+    /**
+     * Method equals.
+     * @param o object to equal
+     * @return true if equal
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        User user = (User) o;
+
+        if (children != user.children) {
+            return false;
+        }
+        if (!name.equals(user.name)) {
+            return false;
+        }
+        return birthday.equals(user.birthday);
     }
 }
