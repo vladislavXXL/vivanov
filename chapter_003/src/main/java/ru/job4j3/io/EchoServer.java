b2fd0d84d8e8a3539c09afbc710632a2f16670ac
VK <vitya39@gmail.com>
2020-04-18 18:57:00 +0300

    Bot

--- chapter_003/src/main/java/ru/job4j3/io/EchoServer.java
+++ chapter_003/src/main/java/ru/job4j3/io/EchoServer.java
@@ -6,9 +6,11 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.function.Consumer;
 
 /**
  * Class EchoServer on socket.
+ *
  * @author v.ivanov
  * @version 1
  * @since 21.03.2020
@@ -16,13 +18,42 @@ import java.net.Socket;
 public class EchoServer {
 
     /**
+     * Enum MSG with request part and corresponding answer.
+     */
+    private enum MSG {
+        /** Hello.*/
+        HELLO("msg=Hello", "Hello"),
+
+        /** Exit.*/
+        EXIT("msg=Exit", "Server finished working");
+
+        /** Message name.*/
+        private String part;
+
+        /** Message answer.*/
+        private String answer;
+
+        /**
+         * Enum MSG constructor.
+         * @param part part of request value
+         * @param answer value to answer
+         */
+        MSG(String part, String answer) {
+            this.part = part;
+            this.answer = answer;
+        }
+    }
+
+    /**
      * Entry method to run program.
+     *
      * @param args args
      * @throws IOException throws exception
      */
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             boolean isClosed = false;
+            Message msg = new Message();
             while (!isClosed) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream(); BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
@@ -31,20 +62,48 @@ public class EchoServer {
                     do {
                         str = in.readLine();
                         System.out.println(str);
-                        String[] arrStr = str.split("\\?");
-                        if (++inc == 1) {
-                            if (str.startsWith("GET") && arrStr.length > 1 && arrStr[1].contains("Bye")) {
-                                isClosed = true;
-                                server.close();
+                        if (str != null) {
+                            String[] arrStr = str.split("\\?");
+                            if (++inc == 1 && arrStr.length > 1) {
+                                String msgPart = arrStr[1].substring(0, arrStr[1].length() - 8).trim();
+                                if (str.startsWith("GET") && msgPart.equals(MSG.EXIT.part)) {
+                                    isClosed = true;
+                                    msg.accept(msgPart);
+                                    server.close();
+                                } else {
+                                    msg.accept(msgPart);
+                                }
                             }
                         }
                     } while (!str.isEmpty());
                     if (!isClosed) {
-                        out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                        out.write("HTTP/1.1 200 OK\r\n".getBytes());
+                        out.write((msg.answer + "\r\n").getBytes());
                     } else {
-                        out.write("HTTP/1.1 503 Service Unavailable\r\n\\".getBytes());
+                        out.write("HTTP/1.1 503 Service Unavailable\r\n".getBytes());
+                        out.write((msg.answer + "\r\n").getBytes());
                     }
                 }
+            }
+        }
+    }
+
+    /**
+     * Class Message.
+     */
+    private static class Message implements Consumer<String> {
+
+        /** Result answer.*/
+        private String answer;
+
+        @Override
+        public void accept(String s) {
+            if (s.equals(MSG.EXIT.part)) {
+                this.answer = MSG.EXIT.answer;
+            } else if (s.equals(MSG.HELLO.part)) {
+                this.answer = MSG.HELLO.answer;
+            } else {
+                this.answer = s.replaceAll("msg=", "");
             }
         }
     }
